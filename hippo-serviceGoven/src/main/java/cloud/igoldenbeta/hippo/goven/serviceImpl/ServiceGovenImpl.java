0787f360c07f0872dbe8051b7f9f50443975a8ac
ruofei <hanrf@igoldenbeta.com>
2016-08-17 10:16:32 +0800

    fix pom

--- hippo-serviceGoven/src/main/java/cloud/igoldenbeta/hippo/goven/serviceImpl/ServiceGovenImpl.java
+++ hippo-serviceGoven/src/main/java/cloud/igoldenbeta/hippo/goven/serviceImpl/ServiceGovenImpl.java
@@ -5,7 +5,7 @@ import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
