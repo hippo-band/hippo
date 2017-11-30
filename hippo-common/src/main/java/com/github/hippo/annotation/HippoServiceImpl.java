87245ed9ad590d110d2541d400cb63760a48d655
sl <bj_ling@aliyun.com>
2017-11-30 14:42:02 +0800

    真*无XML配置

--- hippo-common/src/main/java/com/github/hippo/annotation/HippoServiceImpl.java
+++ hippo-common/src/main/java/com/github/hippo/annotation/HippoServiceImpl.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,10 +17,11 @@ import org.springframework.stereotype.Component;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Component
+@ComponentScan(basePackages = "com.github.hippo")
 public @interface HippoServiceImpl {
   /**
-   * 接口类class
-   * 最新修改是不用传接口.class的,为了兼容老版本+个Default值并声明过期,在未来的某个版本就去掉
+   * 接口类class 最新修改是不用传接口.class的,为了兼容老版本+个Default值并声明过期,在未来的某个版本就去掉
+   * 
    * @return 接口class
    */
   @Deprecated
